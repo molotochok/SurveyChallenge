fd196f1acd08d600b89c81ae07c70113ee3a17b7
Markiian Karpa <markian98@gmail.com>
2019-02-22 22:29:50 +0200

    Added SurveyController with GetSurveys

---
+++ SurveyChallenge/Controllers/Api/SurveyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurveyChallenge.Models;
+
+namespace SurveyChallenge.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveyController : ControllerBase
+    {
+        public readonly ApplicationContext _context;
+
+        public SurveyController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult GetSurveys()
+        {
+            var surveys = _context.Surveys;
+
+            if (surveys == null)
+                return NotFound();
+
+            return Ok(surveys);
+        }
+    }
+}
