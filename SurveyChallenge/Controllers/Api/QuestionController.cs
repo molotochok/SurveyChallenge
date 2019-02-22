c2794106c66095e7b2dfd383f6ef1c68bf79860a
Markiian Karpa <markian98@gmail.com>
2019-02-23 00:06:59 +0200

    Added QuestionController

---
+++ SurveyChallenge/Controllers/Api/QuestionController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using SurveyChallenge.Dtos;
+using SurveyChallenge.Models;
+
+namespace QuestionChallenge.Controllers.Api
+{
+    [Route("api")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        public readonly ApplicationContext _context;
+        public readonly IMapper _mapper;
+
+        public QuestionController(ApplicationContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        // GET /api/questions
+        [HttpGet("questions")]
+        public ActionResult GetQuestions()
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var questions = _context.Questions.Select(m => _mapper.Map<QuestionDto>(m)).ToList();
+
+            return Ok(questions);
+        }
+
+        // GET api/question/{id}
+        [HttpGet("question/{id}")]
+        public ActionResult GetQuestion(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var question = _context.Questions.SingleOrDefault(s => s.Id == id);
+
+            if (question == null)
+                return NotFound();
+
+            return Ok(_mapper.Map<QuestionDto>(question));
+        }
+
+        // POST api/question
+        [HttpPost("question")]
+        public ActionResult PostQuestion(QuestionDto questionDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var question = _mapper.Map<Question>(questionDto);
+
+            _context.Questions.Add(question);
+            _context.SaveChanges();
+
+            questionDto.Id = question.Id;
+            return Created(new Uri(Request.GetDisplayUrl() + "/" + question.Id), questionDto);
+        }
+
+        // PUT api/question/{id}
+        [HttpPut("question/{id}")]
+        public ActionResult PutQuestion(int id, QuestionDto questionDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var questionInDb = _context.Questions.SingleOrDefault(s => s.Id == id);
+
+            if (questionInDb == null)
+                return NotFound();
+
+            questionDto.Id = questionInDb.Id;
+            _mapper.Map(questionDto, questionInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+        // DELETE api/question/{id}
+        [HttpDelete("question/{id}")]
+        public ActionResult DeleteQuestion(int id)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var questionInDb = _context.Questions.SingleOrDefault(s => s.Id == id);
+
+            if (questionInDb == null)
+                return NotFound();
+
+            _context.Questions.Remove(questionInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
