7bdc258dc02ba492bd157fdf809fe32a1f22f24f
Markiian Karpa <markian98@gmail.com>
2019-02-22 21:30:52 +0200

    Added Answer, Survey and Question classes

---
+++ SurveyChallenge/Models/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyChallenge.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Text { get; set; }
+
+        public string Comment { get; set; }
+
+        public List<Answer> Answers { get; set; }
+    }
+}
---
+++ SurveyChallenge/Models/Survey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyChallenge.Models
+{
+    public class Survey
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
+        [Required]
+        [Display(Name = "Author name")]
+        public string AuthorName { get; set; }
+
+        public List<Question> Questions { get; set; }
+    }
+}
