88982a4443faad6420ed722593b169c45e672ab3
Markiian Karpa <markian98@gmail.com>
2019-02-22 21:17:53 +0200

    Added ApplicationContext

---
+++ SurveyChallenge/Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SurveyChallenge.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
